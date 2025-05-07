b1f0c5b834807f2b1658870078ef7998239ec13e
Euge16 <71157100+Euge16@users.noreply.github.com>
2025-05-07 15:23:28 -0300

    feat: implementar pacientes
    - Crear, listar, editar y eliminar pacientes
    - Filtros por nombre, DNI y cobertura médica
    - Autenticación y autorización con JWT y rol 'admin'/'recepcion'
    - Conexión correcta en app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('./db/mongoose');
 
 const usuarioRoutes = require('./rutas/usuario-rutas');
+const pacienteRoutes = require('./rutas/paciente-rutas');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/pacientes', pacienteRoutes);
 
 app.listen(5000);
